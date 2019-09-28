9c6f9a672c753587d234bcbc2d31837aef7557c5
Tiana Johnson <tiana.marie013@gmail.com>
2019-09-28 18:14:36 -0500

    intake build 1

---
+++ src/components/Intake/Intake.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Card from '@material-ui/core/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+
+import './Intake.css';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing(1),
+    },
+}));
+
+class Intake extends Component {
+
+
+    render() {
+        return (
+            <Card className="intake-card">
+                <div className="intake-main">
+                    <FormControl>
+                    <TextField
+                        style={{ margin: 10 }}
+                        className="new-cust-intake"
+                        id="outline-phonenumber"
+                        label="Phone Number"
+                        variant="outlined"
+                        margin="normal"
+                        value={this.state.phone_number}
+                        onChange={this.handleChangePhone}
+                    />
+                    </FormControl>
+                </div>
+            </Card>
+        )
+    }
+}
+
+export default connect(Intake);
