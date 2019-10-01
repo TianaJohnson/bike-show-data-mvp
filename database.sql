c2008492d0294ac988d8e1057b01e177db35224d
Tiana Johnson <tiana.marie013@gmail.com>
2019-10-01 09:38:59 -0500

    frame material table

--- database.sql
+++ database.sql
@@ -8,3 +8,31 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "bike_type" (
+    "id" SERIAL PRIMARY KRY,
+    "road" BOOLEAN,
+    "cyclocross" BOOLEAN,
+    "gravel" BOOLEAN,
+    "moutain" BOOLEAN,
+    "city" BOOLEAN,
+    "track" BOOLEAN,
+    "tandem" BOOLEAN,
+    "experimental" BOOLEAN,
+    "touring" BOOLEAN,
+    "other" VARCHAR (75)
+);
+
+CREATE TABLE "primary_frame_material" (
+    "id" SERIAL PRIMARY KEY,
+    "steel" BOOLEAN,
+    "stainless" BOOLEAN,
+    "carbon" BOOLEAN,
+    "aluminum" BOOLEAN,
+    "titanium" BOOLEAN,
+    "magnesium" BOOLEAN,
+    "wood" BOOLEAN,
+    "bamboo" BOOLEAN,
+    "other" VARCHAR (50)
+
+);
