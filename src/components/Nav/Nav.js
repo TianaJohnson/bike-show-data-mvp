82bf3a460b3eea125c3df6da9b5e5824e82b60e5
Tiana Johnson <tiana.marie013@gmail.com>
2019-10-04 10:22:44 -0500

    css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,7 +8,7 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
+      <h2 className="nav-title">Philly Bike Expo</h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
