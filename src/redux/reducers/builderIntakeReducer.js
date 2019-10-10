477a4be52b02a990da0fe9ce4da99f2b664f2eee
Tiana Johnson <tiana.marie013@gmail.com>
2019-10-10 15:55:03 -0500

    reducer build

--- src/redux/reducers/builderIntakeReducer.js
+++ src/redux/reducers/builderIntakeReducer.js
@@ -1,3 +1,18 @@
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
+
+
+//buider intake SET reducer, interacts w/ saga
+const builderIntakeReducer = (state = [], action) => {
+    console.log('builder intake reducer ');
+      switch (action.type) {
+        case 'SET_BUILDER':
+        console.log('action.paylod:', action.payload)
+          return action.payload;
+      
+        default:
+        console.log('state:', state)
+          return state;
+      }
+    };
 
 export {builderIntakeReducer};
