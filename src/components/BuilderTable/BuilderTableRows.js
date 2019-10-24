0cae8828b9171095d554e089889c85ab3616a40b
Tiana Johnson <tiana.marie013@gmail.com>
2019-10-24 14:09:30 -0500

    table row edit

--- src/components/BuilderTable/BuilderTableRows.js
+++ src/components/BuilderTable/BuilderTableRows.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-//import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 
 class BuilderTableRow extends Component {
 
@@ -13,7 +13,10 @@ class BuilderTableRow extends Component {
                 {console.log('ttttttt', this.props.builders)}
                 <TableRow>
                 <TableCell>{this.props.builders.builder_name}</TableCell>
-                <TableCell>{this.props.builders.brand}</TableCell>
+                <TableCell>{this.props.builders.build_brand}</TableCell>
+                <TableCell><Button variant="contained"
+                        color="secondary"
+                        size="large">file</Button></TableCell>
             </TableRow>
             </div>
         )
