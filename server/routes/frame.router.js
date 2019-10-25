e5dcfc8c9d8ee75970ca86239a5f6a8a071c401a
Tiana Johnson <tiana.marie013@gmail.com>
2019-10-25 14:32:23 -0500

    server side get file build

--- server/routes/frame.router.js
+++ server/routes/frame.router.js
@@ -3,6 +3,28 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
+router.get('/:id', (req, res) => {
+    console.log('in GET file to get id ')
+    console.log('req.params: file get', req.user.id);
+    if (req.isAuthenticated()) {
+        console.log('req.user:', req.user.id);
+        pool.query(`SELECT * FROM "File"
+                    JOIN "builder" ON "bike_file"."builder_id" = "customer_info"."id"
+                    WHERE "bike_file"."builder_id" = $1;`, [req.params.id])
+            .then(results => {
+                console.log(results.rows[0])
+                res.send(results.rows[0])
+            })
+            .catch(error => {
+                console.log('Error making SELECT for project database:', error);
+                res.sendStatus(500);
+            });
+    } else {
+      // They are not authenticated.
+      res.sendStatus(403);
+    }
+  });
+
 
 
 module.exports = router;
